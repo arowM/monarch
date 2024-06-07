be7833d5f9ba4a0e35ee67abf9ed679fbe6c8d1f
Kadzuya Okamoto <arow.okamoto@gmail.com>
2024-06-07 18:12:30 +0900

    Make SendError verbose

--- src/Database/Monarch/Types.hs
+++ src/Database/Monarch/Types.hs
@@ -6,7 +6,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-{- |
+{-|
 Module      : Database.Monarch.Types
 Copyright   : 2013 Noriyuki OHKAWA
 License     : BSD3
@@ -17,23 +17,23 @@ Portability : unknown
 
 Type definitions.
 -}
-module Database.Monarch.Types (
-    Monarch,
-    MonarchT,
-    Connection,
-    ConnectionPool,
-    withMonarchConn,
-    withMonarchPool,
-    runMonarchConn,
-    runMonarchPool,
-    ExtOption (..),
-    RestoreOption (..),
-    MiscOption (..),
-    Code (..),
-    sendLBS,
-    recvLBS,
-    MonadMonarch (..),
-)
+module Database.Monarch.Types
+    ( Monarch
+    , MonarchT
+    , Connection
+    , ConnectionPool
+    , withMonarchConn
+    , withMonarchPool
+    , runMonarchConn
+    , runMonarchPool
+    , ExtOption (..)
+    , RestoreOption (..)
+    , MiscOption (..)
+    , Code (..)
+    , sendLBS
+    , recvLBS
+    , MonadMonarch (..)
+    )
 where
 
 import Control.Monad.Reader (MonadReader, ReaderT (..), asks)
@@ -41,29 +41,47 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import Data.Function
 import Data.Int (Int64)
-import Data.Pool (Pool, defaultPoolConfig, destroyResource, newPool, putResource, takeResource)
-import Network.Socket (
-    AddrInfo (..),
-    AddrInfoFlag (..),
-    HostName,
-    Socket,
-    SocketType (..),
-    close,
-    connect,
-    defaultHints,
-    getAddrInfo,
-    socket,
- )
+import Data.Pool
+    ( Pool
+    , defaultPoolConfig
+    , destroyResource
+    , newPool
+    , putResource
+    , takeResource
+    )
+import Network.Socket
+    ( AddrInfo (..)
+    , AddrInfoFlag (..)
+    , HostName
+    , Socket
+    , SocketType (..)
+    , close
+    , connect
+    , defaultHints
+    , getAddrInfo
+    , socket
+    )
 import qualified Network.Socket.ByteString.Lazy as LBS
 import UnliftIO (MonadIO (liftIO), MonadUnliftIO (withRunInIO))
-import UnliftIO.Exception (Exception, SomeException, bracket, catchAny, mask, onException, throwIO)
+import UnliftIO.Exception
+    ( Exception
+    , SomeException
+    , bracket
+    , catchAny
+    , mask
+    , onException
+    , throwIO
+    )
 import Prelude
+
 
 -- | Connection with TokyoTyrant
 newtype Connection = Connection {connection :: Socket}
 
+
 -- | Connection pool with TokyoTyrant
 type ConnectionPool = Pool Connection
+
 
 -- | Error code
 data Code
@@ -72,13 +90,18 @@ data Code
     | HostNotFound
     | ConnectionRefused
     | SendError
+        { request :: LBS.ByteString
+        , message :: String
+        }
     | ReceiveError
     | ExistingRecord
     | NoRecordFound
     | MiscellaneousError
     deriving (Prelude.Eq, Prelude.Show)
 
+
 instance Exception Code
+
 
 -- | Options for scripting extension
 data ExtOption
@@ -87,15 +110,18 @@ data ExtOption
     | -- | global locking
       GlobalLocking
 
+
 -- | Options for restore
 data RestoreOption
     = -- | consistency checking
       ConsistencyChecking
 
+
 -- | Options for miscellaneous operation
 data MiscOption
     = -- | omission of update log
       NoUpdateLog
+
 
 -- | The Monarch monad transformer to provide TokyoTyrant access.
 newtype MonarchT m a = MonarchT {unMonarchT :: ReaderT Connection m a}
@@ -107,113 +133,130 @@ newtype MonarchT m a = MonarchT {unMonarchT :: ReaderT Connection m a}
         , MonadReader Connection
         )
 
+
 instance (MonadUnliftIO m) => MonadUnliftIO (MonarchT m) where
     withRunInIO inner = MonarchT $ withRunInIO $ \run -> inner (run . unMonarchT)
     {-# INLINE withRunInIO #-}
 
+
 -- | IO Specialized
 type Monarch = MonarchT Prelude.IO
 
+
 -- | Run Monarch with TokyoTyrant at target host and port.
-runMonarch ::
-    ( MonadUnliftIO m
-    ) =>
-    Connection ->
-    MonarchT m a ->
-    m a
+runMonarch
+    :: ( MonadUnliftIO m
+       )
+    => Connection
+    -> MonarchT m a
+    -> m a
 runMonarch conn action =
     runReaderT (unMonarchT action) conn
 
-{- | Create a TokyoTyrant connection and run the given action.
+
+{-| Create a TokyoTyrant connection and run the given action.
 Don't use the given 'Connection' outside the action.
 -}
-withMonarchConn ::
-    ( MonadUnliftIO m
-    ) =>
-    -- | host
-    Prelude.String ->
-    -- | port
-    Prelude.Int ->
-    (Connection -> m a) ->
-    m a
+withMonarchConn
+    :: ( MonadUnliftIO m
+       )
+    => Prelude.String
+    -- ^ host
+    -> Prelude.Int
+    -- ^ port
+    -> (Connection -> m a)
+    -> m a
 withMonarchConn host port = bracket open' close_
-  where
-    open' = liftIO Prelude.$ getConnection host port
-    close_ = liftIO Prelude.. close Prelude.. connection
+    where
+        open' = liftIO Prelude.$ getConnection host port
+        close_ = liftIO Prelude.. close Prelude.. connection
 
-{- | Create a TokyoTyrant connection pool and run the given action.
+
+{-| Create a TokyoTyrant connection pool and run the given action.
 Don't use the given 'ConnectionPool' outside the action.
 -}
-withMonarchPool ::
-    ( MonadIO m
-    ) =>
-    -- | host
-    Prelude.String ->
-    -- | port
-    Prelude.Int ->
-    -- | number of connections
-    Prelude.Int ->
-    (ConnectionPool -> m a) ->
-    m a
+withMonarchPool
+    :: ( MonadIO m
+       )
+    => Prelude.String
+    -- ^ host
+    -> Prelude.Int
+    -- ^ port
+    -> Prelude.Int
+    -- ^ number of connections
+    -> (ConnectionPool -> m a)
+    -> m a
 withMonarchPool host port connections f =
     liftIO
         ( defaultPoolConfig open' close_ 20 connections
             & newPool
         )
         Prelude.>>= f
-  where
-    open' = getConnection host port
-    close_ = close Prelude.. connection
+    where
+        open' = getConnection host port
+        close_ = close Prelude.. connection
+
 
 -- | Run action with a connection.
-runMonarchConn ::
-    ( MonadUnliftIO m
-    ) =>
-    -- | action
-    MonarchT m a ->
-    -- | connection
-    Connection ->
-    m a
+runMonarchConn
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m a
+    -- ^ action
+    -> Connection
+    -- ^ connection
+    -> m a
 runMonarchConn action conn = runMonarch conn action
 
+
 -- | Run action with a unused connection from the pool.
-runMonarchPool ::
-    ( MonadUnliftIO m
-    ) =>
-    -- | action
-    MonarchT m a ->
-    -- | connection pool
-    ConnectionPool ->
-    m a
+runMonarchPool
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m a
+    -- ^ action
+    -> ConnectionPool
+    -- ^ connection pool
+    -> m a
 runMonarchPool action pool = mask $ \unmask -> do
     (res, localPool) <- liftIO $ takeResource pool
-    r <- unmask (runMonarch res action) `onException` liftIO (destroyResource pool localPool res)
+    r <-
+        unmask (runMonarch res action)
+            `onException` liftIO (destroyResource pool localPool res)
     liftIO $ putResource localPool res
     pure r
 
-throwError' ::
-    (MonadIO m) =>
-    Code ->
-    SomeException ->
-    MonarchT m a
+
+throwError'
+    :: (MonadIO m)
+    => Code
+    -> SomeException
+    -> MonarchT m a
 throwError' = Prelude.const Prelude.. throwIO
 
+
 -- | Send.
-sendLBS ::
-    ( MonadUnliftIO m
-    ) =>
-    LBS.ByteString ->
-    MonarchT m ()
+sendLBS
+    :: ( MonadUnliftIO m
+       )
+    => LBS.ByteString
+    -> MonarchT m ()
 sendLBS lbs = do
     conn <- asks connection
-    liftIO (LBS.sendAll conn lbs) `catchAny` throwError' SendError
+    liftIO (LBS.sendAll conn lbs) `catchAny` \e -> do
+        throwIO $
+            SendError
+                { request = lbs
+                , message = show e
+                }
+
 
 -- | Receive.
-recvLBS ::
-    ( MonadUnliftIO m
-    ) =>
-    Int64 ->
-    MonarchT m LBS.ByteString
+recvLBS
+    :: ( MonadUnliftIO m
+       )
+    => Int64
+    -> MonarchT m LBS.ByteString
 recvLBS n = do
     conn <- asks connection
     lbs <- liftIO (LBS.recv conn n) `catchAny` throwError' ReceiveError
@@ -224,216 +267,250 @@ recvLBS n = do
                 then Prelude.return lbs
                 else LBS.append lbs Prelude.<$> recvLBS (n Prelude.- LBS.length lbs)
 
+
 -- | Make connection from host and port.
-getConnection ::
-    HostName ->
-    Prelude.Int ->
-    Prelude.IO Connection
+getConnection
+    :: HostName
+    -> Prelude.Int
+    -> Prelude.IO Connection
 getConnection host port = do
-    let hints =
+    let
+        hints =
             defaultHints
                 { addrFlags = [AI_ADDRCONFIG]
                 , addrSocketType = Stream
                 }
-    (addr : _) <- getAddrInfo (Prelude.Just hints) (Prelude.Just host) (Prelude.Just Prelude.$ Prelude.show port)
+    (addr : _) <-
+        getAddrInfo
+            (Prelude.Just hints)
+            (Prelude.Just host)
+            (Prelude.Just Prelude.$ Prelude.show port)
     sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-    let failConnect = (\e -> close sock Prelude.>> throwIO e) :: SomeException -> Prelude.IO ()
+    let
+        failConnect = (\e -> close sock Prelude.>> throwIO e) :: SomeException -> Prelude.IO ()
     connect sock (addrAddress addr) `catchAny` failConnect
     Prelude.return Prelude.$ Connection sock
+
 
 -- | Monad Monarch interfaces
 class (Prelude.Monad m) => MonadMonarch m where
     -- | Store a record.
     --   If a record with the same key exists in the database,
     --   it is overwritten.
-    put ::
-        -- | key
-        BS.ByteString ->
-        -- | value
-        BS.ByteString ->
-        m ()
+    put
+        :: BS.ByteString
+        -- ^ key
+        -> BS.ByteString
+        -- ^ value
+        -> m ()
+
 
     -- | Store records.
     --   If a record with the same key exists in the database,
     --   it is overwritten.
-    multiplePut ::
-        -- | key & value pairs
-        [(BS.ByteString, BS.ByteString)] ->
-        m ()
+    multiplePut
+        :: [(BS.ByteString, BS.ByteString)]
+        -- ^ key & value pairs
+        -> m ()
+
 
     -- | Store a new record.
     --   If a record with the same key exists in the database,
     --   this function has no effect.
-    putKeep ::
-        -- | key
-        BS.ByteString ->
-        -- | value
-        BS.ByteString ->
-        m ()
+    putKeep
+        :: BS.ByteString
+        -- ^ key
+        -> BS.ByteString
+        -- ^ value
+        -> m ()
+
 
     -- | Concatenate a value at the end of the existing record.
     --   If there is no corresponding record, a new record is created.
-    putCat ::
-        -- | key
-        BS.ByteString ->
-        -- | value
-        BS.ByteString ->
-        m ()
+    putCat
+        :: BS.ByteString
+        -- ^ key
+        -> BS.ByteString
+        -- ^ value
+        -> m ()
+
 
     -- | Concatenate a value at the end of the existing record and shift it to the left.
     --   If there is no corresponding record, a new record is created.
-    putShiftLeft ::
-        -- | key
-        BS.ByteString ->
-        -- | value
-        BS.ByteString ->
-        -- | width
-        Prelude.Int ->
-        m ()
+    putShiftLeft
+        :: BS.ByteString
+        -- ^ key
+        -> BS.ByteString
+        -- ^ value
+        -> Prelude.Int
+        -- ^ width
+        -> m ()
+
 
     -- | Store a record without response.
     --   If a record with the same key exists in the database, it is overwritten.
-    putNoResponse ::
-        -- | key
-        BS.ByteString ->
-        -- | value
-        BS.ByteString ->
-        m ()
+    putNoResponse
+        :: BS.ByteString
+        -- ^ key
+        -> BS.ByteString
+        -- ^ value
+        -> m ()
+
 
     -- | Remove a record.
-    out ::
-        -- | key
-        BS.ByteString ->
-        m ()
+    out
+        :: BS.ByteString
+        -- ^ key
+        -> m ()
+
 
     -- | Remove records.
-    multipleOut ::
-        -- | keys
-        [BS.ByteString] ->
-        m ()
+    multipleOut
+        :: [BS.ByteString]
+        -- ^ keys
+        -> m ()
+
 
     -- | Retrieve a record.
-    get ::
-        -- | key
-        BS.ByteString ->
-        m (Prelude.Maybe BS.ByteString)
+    get
+        :: BS.ByteString
+        -- ^ key
+        -> m (Prelude.Maybe BS.ByteString)
+
 
     -- | Retrieve records.
-    multipleGet ::
-        -- | keys
-        [BS.ByteString] ->
-        m [(BS.ByteString, BS.ByteString)]
+    multipleGet
+        :: [BS.ByteString]
+        -- ^ keys
+        -> m [(BS.ByteString, BS.ByteString)]
+
 
     -- | Get the size of the value of a record.
-    valueSize ::
-        -- | key
-        BS.ByteString ->
-        m (Prelude.Maybe Prelude.Int)
+    valueSize
+        :: BS.ByteString
+        -- ^ key
+        -> m (Prelude.Maybe Prelude.Int)
+
 
     -- | Initialize the iterator.
     iterInit :: m ()
+
 
     -- | Get the next key of the iterator.
     --   The iterator can be updated by multiple connections and then it is not assured that every record is traversed.
     iterNext :: m (Prelude.Maybe BS.ByteString)
 
+
     -- | Get forward matching keys.
-    forwardMatchingKeys ::
-        -- | key prefix
-        BS.ByteString ->
-        -- | maximum number of keys to be fetched. 'Prelude.Nothing' means unlimited.
-        Prelude.Maybe Prelude.Int ->
-        m [BS.ByteString]
+    forwardMatchingKeys
+        :: BS.ByteString
+        -- ^ key prefix
+        -> Prelude.Maybe Prelude.Int
+        -- ^ maximum number of keys to be fetched. 'Prelude.Nothing' means unlimited.
+        -> m [BS.ByteString]
+
 
     -- | Add an integer to a record.
     --   If the corresponding record exists, the value is treated as an integer and is added to.
     --   If no record corresponds, a new record of the additional value is stored.
-    addInt ::
-        -- | key
-        BS.ByteString ->
-        -- | value
-        Prelude.Int ->
-        m Prelude.Int
+    addInt
+        :: BS.ByteString
+        -- ^ key
+        -> Prelude.Int
+        -- ^ value
+        -> m Prelude.Int
+
 
     -- | Add a real number to a record.
     --   If the corresponding record exists, the value is treated as a real number and is added to.
     --   If no record corresponds, a new record of the additional value is stored.
-    addDouble ::
-        -- | key
-        BS.ByteString ->
-        -- | value
-        Prelude.Double ->
-        m Prelude.Double
+    addDouble
+        :: BS.ByteString
+        -- ^ key
+        -> Prelude.Double
+        -- ^ value
+        -> m Prelude.Double
+
 
     -- | Call a function of the script language extension.
-    ext ::
-        -- | function
-        BS.ByteString ->
-        -- | option flags
-        [ExtOption] ->
-        -- | key
-        BS.ByteString ->
-        -- | value
-        BS.ByteString ->
-        m BS.ByteString
+    ext
+        :: BS.ByteString
+        -- ^ function
+        -> [ExtOption]
+        -- ^ option flags
+        -> BS.ByteString
+        -- ^ key
+        -> BS.ByteString
+        -- ^ value
+        -> m BS.ByteString
+
 
     -- | Synchronize updated contents with the file and the device.
     sync :: m ()
 
+
     -- | Optimize the storage.
-    optimize ::
-        -- | parameter
-        BS.ByteString ->
-        m ()
+    optimize
+        :: BS.ByteString
+        -- ^ parameter
+        -> m ()
+
 
     -- | Remove all records.
     vanish :: m ()
 
+
     -- | Copy the database file.
-    copy ::
-        -- | path
-        BS.ByteString ->
-        m ()
+    copy
+        :: BS.ByteString
+        -- ^ path
+        -> m ()
+
 
     -- | Restore the database file from the update log.
-    restore ::
-        (Prelude.Integral a) =>
-        -- | path
-        BS.ByteString ->
-        -- | beginning time stamp in microseconds
-        a ->
-        -- | option flags
-        [RestoreOption] ->
-        m ()
+    restore
+        :: (Prelude.Integral a)
+        => BS.ByteString
+        -- ^ path
+        -> a
+        -- ^ beginning time stamp in microseconds
+        -> [RestoreOption]
+        -- ^ option flags
+        -> m ()
+
 
     -- | Set the replication master.
-    setMaster ::
-        (Prelude.Integral a) =>
-        -- | host
-        BS.ByteString ->
-        -- | port
-        Prelude.Int ->
-        -- | beginning time stamp in microseconds
-        a ->
-        -- | option flags
-        [RestoreOption] ->
-        m ()
+    setMaster
+        :: (Prelude.Integral a)
+        => BS.ByteString
+        -- ^ host
+        -> Prelude.Int
+        -- ^ port
+        -> a
+        -- ^ beginning time stamp in microseconds
+        -> [RestoreOption]
+        -- ^ option flags
+        -> m ()
+
 
     -- | Get the number of records.
     recordNum :: m Int64
 
+
     -- | Get the size of the database.
     size :: m Int64
+
 
     -- | Get the status string of the database.
     status :: m BS.ByteString
 
+
     -- | Call a versatile function for miscellaneous operations.
-    misc ::
-        -- | function name
-        BS.ByteString ->
-        -- | option flags
-        [MiscOption] ->
-        -- | arguments
-        [BS.ByteString] ->
-        m [BS.ByteString]
+    misc
+        :: BS.ByteString
+        -- ^ function name
+        -> [MiscOption]
+        -- ^ option flags
+        -> [BS.ByteString]
+        -- ^ arguments
+        -> m [BS.ByteString]
--- src/Database/Monarch/Utils.hs
+++ src/Database/Monarch/Utils.hs
@@ -1,32 +1,33 @@
 {-# LANGUAGE FlexibleContexts #-}
 
--- |
--- Module      : Database.Monarch.Utils
--- Copyright   : 2013 Noriyuki OHKAWA
--- License     : BSD3
---
--- Maintainer  : n.ohkawa@gmail.com
--- Stability   : experimental
--- Portability : unknown
---
--- Internal utilities.
+{-|
+Module      : Database.Monarch.Utils
+Copyright   : 2013 Noriyuki OHKAWA
+License     : BSD3
+
+Maintainer  : n.ohkawa@gmail.com
+Stability   : experimental
+Portability : unknown
+
+Internal utilities.
+-}
 module Database.Monarch.Utils
-  ( toCode,
-    putMagic,
-    putOptions,
-    lengthBS32,
-    lengthLBS32,
-    fromLBS,
-    yieldRequest,
-    responseCode,
-    parseLBS,
-    parseBS,
-    parseWord32,
-    parseInt64,
-    parseDouble,
-    parseKeyValue,
-    communicate,
-  )
+    ( toCode
+    , putMagic
+    , putOptions
+    , lengthBS32
+    , lengthLBS32
+    , fromLBS
+    , yieldRequest
+    , responseCode
+    , parseLBS
+    , parseBS
+    , parseWord32
+    , parseInt64
+    , parseDouble
+    , parseKeyValue
+    , communicate
+    )
 where
 
 import qualified Data.Binary as B
@@ -39,18 +40,23 @@ import Data.Int
 import Database.Monarch.Types
 import UnliftIO (MonadUnliftIO)
 
+
 class BitFlag32 a where
-  fromOption :: a -> Int32
+    fromOption :: a -> Int32
+
 
 instance BitFlag32 ExtOption where
-  fromOption RecordLocking = 0x1
-  fromOption GlobalLocking = 0x2
+    fromOption RecordLocking = 0x1
+    fromOption GlobalLocking = 0x2
+
 
 instance BitFlag32 RestoreOption where
-  fromOption ConsistencyChecking = 0x1
+    fromOption ConsistencyChecking = 0x1
+
 
 instance BitFlag32 MiscOption where
-  fromOption NoUpdateLog = 0x1
+    fromOption NoUpdateLog = 0x1
+
 
 -- | Convert status code
 toCode :: Int -> Code
@@ -58,162 +64,185 @@ toCode 0 = Success
 toCode 1 = InvalidOperation
 toCode 2 = HostNotFound
 toCode 3 = ConnectionRefused
-toCode 4 = SendError
+toCode 4 =
+    SendError
+        { request = LBS.empty
+        , message = ""
+        }
 toCode 5 = ReceiveError
 toCode 6 = ExistingRecord
 toCode 7 = NoRecordFound
 toCode 9999 = MiscellaneousError
 toCode _ = error "Invalid Code"
 
--- | TokyoTyrant Original Binary Protocal magic id.
---
--- Example:
---
--- >>> :m +Data.ByteString.Char8
--- >>> :set -XOverloadedStrings
--- >>> fromLBS (runPut $ putMagic 0x10) == "\xC8\x10"
--- True
+
+{-| TokyoTyrant Original Binary Protocal magic id.
+
+Example:
+
+>>> :m +Data.ByteString.Char8
+>>> :set -XOverloadedStrings
+>>> fromLBS (runPut $ putMagic 0x10) == "\xC8\x10"
+True
+-}
 putMagic :: B.Word8 -> B.Put
 putMagic magic = B.putWord8 0xC8 >> B.putWord8 magic
 
--- | Option
---
--- Example:
---
--- >>> :m +Data.ByteString.Char8
--- >>> :set -XOverloadedStrings
--- >>> fromLBS (runPut $ putOptions [RecordLocking]) == "\0\0\0\1"
--- True
--- >>> fromLBS (runPut $ putOptions [GlobalLocking]) == "\0\0\0\2"
--- True
--- >>> fromLBS (runPut $ putOptions [RecordLocking, GlobalLocking]) == "\0\0\0\3"
--- True
-putOptions ::
-  (BitFlag32 option) =>
-  [option] ->
-  B.Put
-putOptions =
-  putWord32be
-    . fromIntegral
-    . foldl (.|.) 0
-    . map fromOption
 
--- | Get Length
---
--- Example:
---
--- >>> :m +Data.ByteString.Char8
--- >>> :set -XOverloadedStrings
--- >>> lengthBS32 "test"
--- 4
--- >>> lengthBS32 ""
--- 0
+{-| Option
+
+Example:
+
+>>> :m +Data.ByteString.Char8
+>>> :set -XOverloadedStrings
+>>> fromLBS (runPut $ putOptions [RecordLocking]) == "\0\0\0\1"
+True
+>>> fromLBS (runPut $ putOptions [GlobalLocking]) == "\0\0\0\2"
+True
+>>> fromLBS (runPut $ putOptions [RecordLocking, GlobalLocking]) == "\0\0\0\3"
+True
+-}
+putOptions
+    :: (BitFlag32 option)
+    => [option]
+    -> B.Put
+putOptions =
+    putWord32be
+        . fromIntegral
+        . foldl (.|.) 0
+        . map fromOption
+
+
+{-| Get Length
+
+Example:
+
+>>> :m +Data.ByteString.Char8
+>>> :set -XOverloadedStrings
+>>> lengthBS32 "test"
+4
+>>> lengthBS32 ""
+0
+-}
 lengthBS32 :: BS.ByteString -> B.Word32
 lengthBS32 = fromIntegral . BS.length
 
--- | Get Length
---
--- Example:
---
--- >>> :m +Data.ByteString.Lazy.Char8
--- >>> :set -XOverloadedStrings
--- >>> lengthLBS32 "test"
--- 4
--- >>> lengthLBS32 ""
--- 0
+
+{-| Get Length
+
+Example:
+
+>>> :m +Data.ByteString.Lazy.Char8
+>>> :set -XOverloadedStrings
+>>> lengthLBS32 "test"
+4
+>>> lengthLBS32 ""
+0
+-}
 lengthLBS32 :: LBS.ByteString -> B.Word32
 lengthLBS32 = fromIntegral . LBS.length
 
--- | Convert
---
--- Example:
---
--- >>> :m +Data.ByteString.Lazy.Char8
--- >>> :set -XOverloadedStrings
--- >>> fromLBS "test"
--- "test"
+
+{-| Convert
+
+Example:
+
+>>> :m +Data.ByteString.Lazy.Char8
+>>> :set -XOverloadedStrings
+>>> fromLBS "test"
+"test"
+-}
 fromLBS :: LBS.ByteString -> BS.ByteString
 fromLBS = BS.pack . LBS.unpack
 
+
 -- | Send request.
-yieldRequest ::
-  ( MonadUnliftIO m
-  ) =>
-  B.Put ->
-  MonarchT m ()
+yieldRequest
+    :: ( MonadUnliftIO m
+       )
+    => B.Put
+    -> MonarchT m ()
 yieldRequest = sendLBS . runPut
 
+
 -- | Receive response code.
-responseCode ::
-  ( MonadUnliftIO m
-  ) =>
-  MonarchT m Code
+responseCode
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m Code
 responseCode = toCode . fromIntegral . runGet B.getWord8 <$> recvLBS 1
 
+
 -- | Parse byte string (lazy) value.
-parseLBS ::
-  ( MonadUnliftIO m
-  ) =>
-  MonarchT m LBS.ByteString
+parseLBS
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m LBS.ByteString
 parseLBS =
-  recvLBS 4
-    >>= recvLBS . fromIntegral . runGet getWord32be
+    recvLBS 4
+        >>= recvLBS . fromIntegral . runGet getWord32be
+
 
 -- | Parse byte string (strict) value.
-parseBS ::
-  ( MonadUnliftIO m
-  ) =>
-  MonarchT m BS.ByteString
+parseBS
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m BS.ByteString
 parseBS = fromLBS <$> parseLBS
 
+
 -- | Parse Word32 value.
-parseWord32 ::
-  ( MonadUnliftIO m
-  ) =>
-  MonarchT m B.Word32
+parseWord32
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m B.Word32
 parseWord32 = runGet getWord32be <$> recvLBS 4
 
+
 -- | Parse Int64 value.
-parseInt64 ::
-  ( MonadUnliftIO m
-  ) =>
-  MonarchT m Int64
+parseInt64
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m Int64
 parseInt64 = runGet (B.get :: B.Get Int64) <$> recvLBS 8
 
+
 -- | Parse Double value.
-parseDouble ::
-  ( MonadUnliftIO m
-  ) =>
-  MonarchT m Double
+parseDouble
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m Double
 parseDouble = do
-  integ <- fromIntegral <$> parseInt64
-  fract <- fromIntegral <$> parseInt64
-  return $ integ + fract * 1e-12
+    integ <- fromIntegral <$> parseInt64
+    fract <- fromIntegral <$> parseInt64
+    return $ integ + fract * 1e-12
+
 
 -- | Parse key value pair.
-parseKeyValue ::
-  ( MonadUnliftIO m
-  ) =>
-  MonarchT m (BS.ByteString, BS.ByteString)
+parseKeyValue
+    :: ( MonadUnliftIO m
+       )
+    => MonarchT m (BS.ByteString, BS.ByteString)
 parseKeyValue = do
-  ksiz <- recvLBS 4
-  vsiz <- recvLBS 4
-  key <-
-    recvLBS . fromIntegral $
-      runGet getWord32be ksiz
-  value <-
-    recvLBS . fromIntegral $
-      runGet getWord32be vsiz
-  return (fromLBS key, fromLBS value)
+    ksiz <- recvLBS 4
+    vsiz <- recvLBS 4
+    key <-
+        recvLBS . fromIntegral $
+            runGet getWord32be ksiz
+    value <-
+        recvLBS . fromIntegral $
+            runGet getWord32be vsiz
+    return (fromLBS key, fromLBS value)
+
 
 -- | Make a query.
-communicate ::
-  ( MonadUnliftIO m
-  ) =>
-  B.Put ->
-  (Code -> MonarchT m a) ->
-  MonarchT m a
+communicate
+    :: ( MonadUnliftIO m
+       )
+    => B.Put
+    -> (Code -> MonarchT m a)
+    -> MonarchT m a
 communicate makeRequest makeResponse =
-  yieldRequest makeRequest
-    >> responseCode
-    >>= makeResponse
+    yieldRequest makeRequest
+        >> responseCode
+        >>= makeResponse
